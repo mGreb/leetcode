3901a6c2a3d38dd1e91a9e53ded75bc52d7f4f2b
mgreb <mgreb92@gmail.com>
2022-05-29 23:28:58 +0500

    Task 0035 added

---
+++ 0035/0035.cpp
@@ -0,0 +1,66 @@
+/*35. Search Insert Position
+ * Easy
+ * Given a sorted array of distinct integers and a target value,
+ * return the index if the target is found.
+ * If not, return the index where it would be
+ * if it were inserted in order.
+ * You must write an algorithm with O(log n) runtime complexity.
+ *
+ * Constraints:
+ * 1 <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums contains distinct values sorted in ascending order.
+ * -10^4 <= target <= 10^4
+*/
+
+#include <vector>
+
+class Solution {
+public:
+  int searchInsert(const std::vector<int>& nums, int target) {
+    const size_t n = nums.size();
+    
+    if (n == 0 || target < nums[0])
+      return 0;
+    if (target > nums.back())
+      return n;
+    
+    size_t left = 0, mid = n / 2, right = n - 1;
+    while (true) {
+      if (nums[left] <= target && nums[mid] > target)
+        right = mid;
+      else
+        left = mid;
+      
+      mid = left + (right - left) / 2;
+      
+      if (nums[mid] == target)
+        return mid;
+      
+      if (right - left <= 1)
+        return right;
+    }
+  }
+};
+
+int main() {
+	Solution s;
+  
+  const bool res12 = s.searchInsert({1,3,5}, 4) == 2;
+  const bool res0 = s.searchInsert({1,4,6,7,8,9}, 6) == 2;
+  
+  const bool res1 = s.searchInsert({1,3,5,6}, 5) == 2;
+	const bool res2 = s.searchInsert({1,3,5,6}, 2) == 1;
+	const bool res3 = s.searchInsert({1,3,5,6}, 7) == 4;
+	const bool res4 = s.searchInsert({1,3,5,6}, -10) == 0;
+ 
+	const bool res5 = s.searchInsert({1,3,5,6}, 1) == 0;
+	const bool res6 = s.searchInsert({1,3,5,6}, 3) == 1;
+	const bool res8 = s.searchInsert({1,3,5,6}, 6) == 3;
+ 
+	const bool res9  = s.searchInsert({1}, 6) == 1;
+	const bool res10 = s.searchInsert({1}, 1) == 0;
+	const bool res11 = s.searchInsert({1}, 0) == 0;
+ 
+	return 0;
+}
