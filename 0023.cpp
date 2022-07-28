c3cabf41ee0334d44210630f84b546662de74b1a
mgreb <mgreb92@gmail.com>
2022-07-28 21:54:12 +0500

    Fastest 0023

--- 0023.cpp
+++ 0023.cpp
@@ -1,8 +1,26 @@
-/*
- * 23. Merge k Sorted Lists
+/* 23. Merge k Sorted Lists
  * You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
  * Merge all the linked-lists into one sorted linked-list and return it.
- */
+ * Example 1:
+ *  Input: lists = [[1,4,5],[1,3,4],[2,6]]
+ *  Output: [1,1,2,3,4,4,5,6]
+ *  Explanation: The linked-lists are:
+ *  [1->4->5, 1->3->4, 2->6]
+ *  merging them into one sorted list:
+ *  1->1->2->3->4->4->5->6
+ * Example 2:
+ *  Input: lists = []
+ *  Output: []
+ * Example 3:
+ *  Input: lists = [[]]
+ *  Output: []
+ * Constraints:
+ *  k == lists.length
+ *  0 <= k <= 10^4
+ *  0 <= lists[i].length <= 500
+ *  -10^4 <= lists[i][j] <= 10^4
+ *  lists[i] is sorted in ascending order.
+ *  The sum of lists[i].length will not exceed 10^4*/
 
 #include <vector>
 #include <set>
@@ -12,23 +30,55 @@ struct ListNode {
      int val;
      ListNode *next;
      ListNode() : val(0), next(nullptr) {}
-     ListNode(int x) : val(x), next(nullptr) {}
+     explicit ListNode(int x) : val(x), next(nullptr) {}
      ListNode(int x, ListNode *next) : val(x), next(next) {}
  };
 
+// just cram everything into vector and sort it!
+// such wow, so much faster
+// although not memory friendly
 class Solution {
+public:
+  static ListNode* mergeKLists(const std::vector<ListNode*>& lists) {
+    std::vector<std::pair<int, ListNode*>> v;
+    
+    for (auto item: lists) {
+      while (item != nullptr) {
+        v.push_back({item->val, item});
+        item = item->next;
+      }
+    }
+    
+    if (v.empty())
+      return nullptr;
+    
+    std::sort(v.begin(), v.end(), [](const std::pair<int, ListNode*>& a,
+                                     const std::pair<int, ListNode*>& b)
+                                     {return a.first < b.first;});
+    
+    for (size_t i = 0; i < v.size() - 1; ++i) {
+      v[i].second->next = v[i+1].second;
+    }
+    v.back().second->next = nullptr;
+    
+    return v[0].second;
+  }
+};
+
+// priority queue, still slow
+class Solution2 {
   struct comp {
     bool operator()(const std::pair<int, ListNode*>& a, const std::pair<int, ListNode*>& b) const {
       return a.first > b.first;
     }
   };
 public:
-  ListNode* mergeKLists(std::vector<ListNode*>& lists) {
+  static ListNode* mergeKLists(const std::vector<ListNode*>& lists) {
     std::priority_queue<std::pair<int, ListNode*>, std::vector<std::pair<int, ListNode*>>, comp> my_pr_queue;
     for (auto l : lists)
       if (l != nullptr)
         my_pr_queue.push({l->val, l});
-    ListNode* temp_head = new ListNode(0);
+    auto temp_head = new ListNode(0);
     ListNode* curr = temp_head;
     while (!my_pr_queue.empty()) {
       ListNode* smallest = my_pr_queue.top().second;
@@ -36,7 +86,7 @@ public:
       curr->next = smallest;
       curr = curr->next;
       
-      my_pr_queue.pop();  // my_pr_queue.begin());
+      my_pr_queue.pop();
       if (smallest->next != nullptr)
         my_pr_queue.push({smallest->next->val, smallest->next});
     }
@@ -46,19 +96,20 @@ public:
   }
 };
 
-class Solution0 {
+// based on multiset, pretty slow but short
+class Solution1 {
   struct comp {
     bool operator()(const std::pair<int, ListNode*>& a, const std::pair<int, ListNode*>& b) const {
       return a.first < b.first;
     }
   };
 public:
-  ListNode* mergeKLists(std::vector<ListNode*>& lists) {
+  static ListNode* mergeKLists(const std::vector<ListNode*>& lists) {
     std::multiset<std::pair<int, ListNode*>, comp> my_set;
     for (auto l : lists)
       if (l != nullptr)
         my_set.insert({l->val, l});
-    ListNode* temp_head = new ListNode(0);
+    auto temp_head = new ListNode(0);
     ListNode* curr = temp_head;
     while (!my_set.empty()) {
       ListNode* smallest = my_set.begin()->second;
@@ -76,8 +127,9 @@ public:
   }
 };
 
-class Solution1 {
-	int getMinListNodePos(std::vector<ListNode*>& lists) {
+// old, complicated and slow
+class Solution0 {
+	static int getMinListNodePos(const std::vector<ListNode*>& lists) {
 		
 		// find first non-null element
 		int minVal = 0;
