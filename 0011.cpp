26989040396040d4e764ad2579e27a59c9758306
mgreb <mgreb92@gmail.com>
2022-07-31 20:53:56 +0500

    Task 0011 added

---
+++ 0011.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+class Solution {
+public:
+  int maxArea(std::vector<int>& height) {
+    int max = 0;
+    int l = 0, r = height.size() - 1;
+    while (l < r) {
+      max = std::max(max, std::min(height[l], height[r]) * (r - l));
+      if (height[l] < height[r])
+        ++l;
+      else
+        --r;
+    }
+    return max;
+  }
+};
